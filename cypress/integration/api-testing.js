5f861da4fb2e2adc0f6c845042badc64bafa0078
Arun-Ganesh07 <arun.gan1234@gmail.com>
2022-05-28 15:15:10 -0700

    finishing Automated Acceptance Testing

--- cypress/integration/api-testing.js
+++ cypress/integration/api-testing.js
@@ -32,23 +32,64 @@ describe("Backend (REST API) is listening", () => {
           //OR use another set of supported matchers from Chai, the Expect style:
           //https://www.chaijs.com/guide/styles/#expect
           expect(response.body).to.be.a("object");
+
           //arr = response.body
           //arr.ForEach((elem) => expect(response.body).to.property('{ Object (userData) }'))
-          //expect(response.body).to.deep.equal('{ Object (userData) }');
+          //expect(response.body).to.match('userData');
           expect(response.body).to.exist;
-          // expect(response.body).to.deep.equal({"userData":[{"_id":"62756a5e83a398b7749c48ce",
-          // "email":"something@email.com","password":"$2a$08$wYXmS9I7XeRIAvFodYf15uZvTVWNDpD/qYKTH/MhbM4VFrNrl3pGC",
-          // "administrator":false,"__v":0},{"_id":"627d5210cd5401e489551b2a","email":"test@gmail.com",
-          // "password":"$2a$10$rncAYvw2lUiCBe58tlXjfuBBZ9yQXJVG7uYn6KSlCfgiyiQS.WVU2","administrator":false,"__v":0},
-          // {"_id":"627d530c72a4c31a0d16ac42","email":"tester@gmail.com","password":"$2a$10$NqcnTTgT6SDcCPWgl/gnqeNLPHxwbn9Z0sNmCa42sTfr1YE4Hj4x.",
-          // "administrator":false,"__v":0},{"_id":"628536e5d9a4447f1b7235da","email":"doggy@gmail.com","password":"$2a$08$3X..JSir37u/TvVGGz7paOzaDjtAti2DefMs9eCgJPFB6zdV66HYi",
-          // "administrator":false,"__v":0},{"_id":"6285376bd9a4447f1b7235e0","email":"doggys@gmail.com","password":"$2a$08$UnmkCxMkxk7wboRKrbyr5OcRAPHTRkEcPcwZ/8uNLd3ZFWqbqXjN2",
-          // "administrator":false,"__v":0}]});
-          expect(response.body).to.be.instanceOf("userData");
+          expect(response.body).to.deep.equal({
+            userData: [
+              {
+                _id: "62756a5e83a398b7749c48ce",
+                email: "something@email.com",
+                password:
+                  "$2a$08$wYXmS9I7XeRIAvFodYf15uZvTVWNDpD/qYKTH/MhbM4VFrNrl3pGC",
+                administrator: false,
+                __v: 0,
+              },
+              {
+                _id: "627d5210cd5401e489551b2a",
+                email: "test@gmail.com",
+                password:
+                  "$2a$10$rncAYvw2lUiCBe58tlXjfuBBZ9yQXJVG7uYn6KSlCfgiyiQS.WVU2",
+                administrator: false,
+                __v: 0,
+              },
+              {
+                _id: "627d530c72a4c31a0d16ac42",
+                email: "tester@gmail.com",
+                password:
+                  "$2a$10$NqcnTTgT6SDcCPWgl/gnqeNLPHxwbn9Z0sNmCa42sTfr1YE4Hj4x.",
+                administrator: false,
+                __v: 0,
+              },
+              {
+                _id: "628536e5d9a4447f1b7235da",
+                email: "doggy@gmail.com",
+                password:
+                  "$2a$08$3X..JSir37u/TvVGGz7paOzaDjtAti2DefMs9eCgJPFB6zdV66HYi",
+                administrator: false,
+                __v: 0,
+              },
+              {
+                _id: "6285376bd9a4447f1b7235e0",
+                email: "doggys@gmail.com",
+                password:
+                  "$2a$08$UnmkCxMkxk7wboRKrbyr5OcRAPHTRkEcPcwZ/8uNLd3ZFWqbqXjN2",
+                administrator: false,
+                __v: 0,
+              },
+            ],
+          });
+          //expect(response.body).to.be.instanceOf;
           // assert.to.equal(
           //  response.body,
           //   "Object (userData)"
           // );
+
+          // var arr = response.body;
+          // arr.forEach(elem => expect(reponse.body).to.equal("Object (userData)"))
+
           expect(response.status).to.equal(200);
         }
       );
@@ -76,45 +117,44 @@ describe("Backend (REST API) is listening", () => {
  */
 
 describe("API takes an obj and adds it to the DB", () => {
-  context("Successfull get", () => {
+  context("Successful get", () => {
     before(() => {});
 
     let user = {};
 
     it("GIVEN My user object has valid fields (user and job)", () => {
       user = {
-        email: "doggys@gmail.com",
+        email: undefined,
         password:
           "$2a$08$UnmkCxMkxk7wboRKrbyr5OcRAPHTRkEcPcwZ/8uNLd3ZFWqbqXjN2",
       };
     });
 
     it("WHEN I attempt to post the user obj", () => {
-      cy.request("GET", "https://dog-grooming-api.herokuapp.com/", user).then(
-        (response) => {
-          //Using matchers from Chai: https://www.chaijs.com/guide/styles/#assert
-          // All Cypress supported matchers here: https://docs.cypress.io/guides/references/assertions
-          assert.equal(
-            response.status,
-            200,
-            "THEN I receive a successful response (code 200)"
-          );
-          //   assert.exists(
-          //     response.body.email,
-          //     "AND the response object contains the property _id"
-          //   );
-          assert.equal(
-            response.body.email,
-            user.email,
-            "AND the response object contains the same name and job I passed"
-          );
-          assert.equal(
-            response.body.password,
-            user.passwrod,
-            "AND the response object contains the same name and job I passed"
-          );
-        }
-      );
+      cy.request(
+        "GET",
+        "https://dog-grooming-api.herokuapp.com/users",
+        user
+      ).then((response) => {
+        //Using matchers from Chai: https://www.chaijs.com/guide/styles/#assert
+        // All Cypress supported matchers here: https://docs.cypress.io/guides/references/assertions
+        assert.equal(
+          response.status,
+          200,
+          "THEN I receive a successful response (code 200)"
+        );
+
+        assert.equal(
+          response.body.email,
+          user.email,
+          "AND the response object contains the same name and job I passed"
+        );
+        assert.equal(
+          response.body.password,
+          user.passwordd,
+          "AND the response object contains the same name and job I passed"
+        );
+      });
     });
   });
 
@@ -125,8 +165,8 @@ describe("API takes an obj and adds it to the DB", () => {
 
     it("GIVEN My user object has an invalid field (job)", () => {
       user = {
-        name: "Leah",
-        job: "S",
+        name: "Arun",
+        job: "G",
       };
     });
 
@@ -140,7 +180,6 @@ describe("API takes an obj and adds it to the DB", () => {
         //Using matchers from Chai: https://www.chaijs.com/guide/styles/#assert
         //All Cypress supported matchers here: https://docs.cypress.io/guides/references/assertions
         assert.equal(response.status, 401, "No token");
-        //assert.notExists('No token');
       });
     });
   });
--- cypress/integration/e2e-testing.js
+++ cypress/integration/e2e-testing.js
@@ -13,7 +13,7 @@
  */
 
 describe("Add new user", () => {
-  context("Successfull post", () => {
+  context("Successful post", () => {
     it("GIVEN I navigate to the add user page", () => {
       cy.visit("https://dog-groomings.herokuapp.com/");
     });
@@ -23,62 +23,41 @@ describe("Add new user", () => {
         "addUser"
       );
       cy.get("form").within(() => {
-        cy.get('input[name="_id"]').type("6285376bd9a4447f1b7235e0");
-        cy.get('input[name="email"]').type("doggys@gmail.com");
-        cy.get('input[name="password"]').type(
-          "$2a$08$UnmkCxMkxk7wboRKrbyr5OcRAPHTRkEcPcwZ/8uNLd3ZFWqbqXjN2"
-        );
-        cy.get('input[name="administrator"]').type("false");
-        cy.get('input[name="__v"]').type("0");
-        //cy.get('input[type="doggys@gmail.com"]').click();
-        //Or
-        cy.contains("Sign In").click();
+        cy.get("input.form-control").eq(0).type("doggys@gmail.com");
+        cy.get("input.form-control").eq(1).type("ThisPassword1");
+
+        cy.get("button.btn.btn-primary").eq(0).click();
       });
-      cy.wait("@addUser");
     });
 
-    it("THEN a new user is added to the table with the given name and job", () => {
-      cy.visit("https://dog-groomings.herokuapp.com/");
-      cy.get("tbody").find("tr").as("rows");
-      cy.get("@rows").last().should("contain", "Pamela");
-      cy.get("@rows").last().should("contain", "Sw Eng");
+    it("THEN the user is brought to the dashboard with any upcoming appointments if available", () => {
+      cy.visit("https://dog-groomings.herokuapp.com/dashboard");
+      cy.get("div.card-body").should("contain", "No Upcoming Appointments");
     });
   });
 });
 
-//   context("Unsuccessfull post", () => {
-//     let rowsLengthBefore = 0;
-//     before(() => {
-//       cy.visit("http://localhost:3000/users-table");
-//       cy.get("tbody")
-//         .find("tr")
-//         .then(($rows) => {
-//           rowsLengthBefore = $rows.length;
-//         });
-//     });
+context("Unsuccessful post", () => {
+  it("GIVEN I navigate to the add user page", () => {
+    cy.visit("https://dog-groomings.herokuapp.com/");
+  });
 
-//     it("GIVEN I navigate to the add user page", () => {
-//       cy.visit("http://localhost:3000/form");
-//     });
+  it("WHEN I enter user and job and submit the form", () => {
+    cy.intercept("POST", "https://dog-grooming-api.herokuapp.com/users").as(
+      "addUser"
+    );
+    cy.get("form").within(() => {
+      //cy.get('div.row').type("something@email.com");
+      cy.get("input.form-control").eq(0).type("doggyswrong@gmail.com");
+      cy.get("input.form-control").eq(1).type("asdfasdaf");
 
-//     it("WHEN I enter user and and invalid job (1 char only) and submit the form", () => {
-//       cy.intercept("POST", "http://localhost:5000/users").as("addUser");
-//       cy.get("form").within(() => {
-//         cy.get('input[name="name"]').type("Pamela");
-//         cy.get('input[name="job"]').type("S");
-//         cy.get('input[type="button"]').click();
-//         //Or
-//         //cy.contains('Submit').click()
-//       });
-//       cy.wait("@addUser");
-//     });
+      cy.get("button.btn.btn-primary").eq(0).click();
+    });
 
-//     it("THEN the user is not added to the table", () => {
-//       cy.visit("http://localhost:3000/users-table");
-//       cy.get("tbody")
-//         .find("tr")
-//         .as("rows")
-//         .should("have.length", rowsLengthBefore);
-//     });
-//   });
-// });
+    //cy.wait("@addUser");
+  });
+
+  it("THEN the user receives an error message", () => {
+    cy.get("i").eq(0).should("contain", "Not authorized");
+  });
+});
